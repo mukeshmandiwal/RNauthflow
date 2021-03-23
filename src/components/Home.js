ee00381542e924cb53540c742d7d4c270cb39f50
Mukesh Mandiwal <mkmandiwal@tekion.com>
2021-03-23 20:15:23 +0530

    Add stack screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,41 @@
 import * as React from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {useAuthorization} from './AuthProvider';
 
-const SignOutButton = () => {
+const Stack = createStackNavigator();
+
+const SignOutButton = ({navigation}) => {
   const {signOut} = useAuthorization();
-  return <Button title="Sign Out" onPress={() => signOut('')} />;
+
+  function navigateAuth() {
+    signOut();
+    navigation.navigate('Home');
+  }
+  return <Button title="Sign Out" onPress={navigateAuth} />;
 };
 
-const SignInButton = () => {
+const SignInButton = ({navigation}) => {
   const {signIn} = useAuthorization();
-  return <Button title="Sign IN" onPress={() => signIn('my_token')} />;
+  function navigateHome() {
+    signIn('my_token');
+    navigation.navigate('Auth');
+  }
+  return <Button title="Sign IN" onPress={navigateHome} />;
 };
-export const Home = () => {
-  const {status, userToken} = useAuthorization();
+export const HomeScreen = props => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.actions}>
+        <SignInButton {...props} />
+      </View>
+    </View>
+  );
+};
 
+export const LoginScreen = props => {
+  const {status, userToken} = useAuthorization();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>status : {status}</Text>
@@ -21,12 +43,26 @@ export const Home = () => {
         userToken : {userToken ? userToken : 'null'}
       </Text>
       <View style={styles.actions}>
-        <SignInButton />
-        <SignOutButton />
+        <SignOutButton {...props} />
       </View>
     </View>
   );
 };
+
+export function Home() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          initialRouteName="Auth"
+          name="Auth"
+          component={LoginScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
