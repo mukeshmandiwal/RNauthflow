36caccde6c074f41367b4da7505eddbb58105f17
Mukesh Mandiwal <mkmandiwal@tekion.com>
2021-04-27 00:48:04 +0530

    Remove deprected npm package

--- src/components/async-storage.js
+++ src/components/async-storage.js
@@ -1,6 +1,6 @@
-import AsyncStorage from '@react-native-community/async-storage';
-export async function getItem(key) {
-  const value = await AsyncStorage.getItem(key);
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export async function getItem() {
+  const value = await AsyncStorage.getItem('token');
   return value ? JSON.parse(value) : null;
 }
 export async function setItem(value) {
